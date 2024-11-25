ff1555bf6b661678115414351178ef5002366779
unknown <victormirescu2000@gmail.com>
2024-11-26 00:04:50 +0200

    add get email list and send email methods

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import LandingPage from './components/LandingPage';
 import './styles.css';
 import { CSPostHogProvider } from './providers'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SendNewsletter from './components/SendNewsletter';
 
 function App() {
+  // return (
+  //   // <CSPostHogProvider>
+
+  //     <div>
+  //       <LandingPage />
+  //     </div>
+  //   // </CSPostHogProvider>
+  // );
   return (
-    // <CSPostHogProvider>
-      <div>
-        <LandingPage />
-      </div>
-    // </CSPostHogProvider>
+    <Router>
+      <Routes>
+        <Route path="/landing" element={<LandingPage />}/>
+        <Route path="/sendN" element={<SendNewsletter />} />
+      </Routes>
+    </Router>
   );
 }
 
