b42dc10b672b8a728569deeb096c2fda5eee3d58
Alex-tech17 <alex.valcu2001@yahoo.com>
2024-11-29 18:53:40 +0200

    Changed posthog project

--- src/providers.js
+++ src/providers.js
@@ -1,6 +1,6 @@
 import posthog from 'posthog-js'
 
-posthog.init('phc_I3HhE8gZufbartT15PrEjeJo3GVGJ5S4Dl6jN1SBa1',
+posthog.init('phc_8smOkLccJGmGvpAWV79oJwJpEIqVLt3HZao5yAKLZR7',
     {
         api_host: 'https://eu.i.posthog.com',
         person_profiles: 'always' // or 'always' to create profiles for anonymous users as well
