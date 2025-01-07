2a5829a929d7a3d606396918dd6d713377fa9a27
andreeas11 <bianca.stefan1110@gmail.com>
2025-01-07 10:57:52 +0200

    add link to app

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
 import NewsletterPopup from "./Newsletter"; // Import your popup component
+import posthog from "posthog-js";
 
 export default function Hero() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -12,21 +13,34 @@ export default function Hero() {
     });
   };
 
+  const redirectToApp = () => {
+      posthog.capture("get app", { property: "value" });
+      window.location.href = "https://we.tl/t-6VEtIHF430";
+  };
+
   return (
     <section className="hero">
       <h1 style={{ color: "#ffffff", textShadow: "1px 1px 4px rgba(0, 0, 0, 0.5)", marginLeft: "-45vw" }}>
         Welcome to Quick Fix
       </h1>
       <p style={{ color: "#ffffff", textShadow: "1px 1px 4px rgba(0, 0, 0, 0.5)", marginLeft: "-45vw" }}>
-      Find Trusted Service Providers, Anytime, Anywhere
+        Find Trusted Service Providers, Anytime, Anywhere
       </p>
-      {/* <button className="cta-button" style={{ marginLeft: "-45vw"}}>Get Started</button> */}
-      
-      <button className="cta-button" style={{ marginLeft: "-45vw"}}
-        onClick={() => setIsPopupOpen(true)} // Open popup on click
-      >
-        Get Started
-      </button>
+
+      {/* Button Container */}
+      <div style={{ display: "flex", gap: "1rem", marginLeft: "-45vw" }}>
+        <button className="cta-button"
+          onClick={() => setIsPopupOpen(true)} // Open popup on click
+        >
+          Get Started
+        </button>
+        <button className="cta-button"
+          onClick={redirectToApp} // Redirect to the app link
+        >
+          Get App
+        </button>
+      </div>
+
       <NewsletterPopup
         isPopupOpen={isPopupOpen}
         setIsPopupOpen={setIsPopupOpen}
